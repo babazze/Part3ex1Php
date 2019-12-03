b472c5b93477b9883261f08512086f7c22ccc970
babazze <babazze@live.fr>
2019-12-03 10:58:09 +0100

    fini

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$months = array (1 => 'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Decembre');
+?>
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>ex1</title>
+  </head>
+  <body>
+      <p>
+        <?php
+          echo $months[5]
+        ?>
+      </p>
+  </body>
+</html>
